d831d9150195ed68f3da405aa194b2e5b759f65e
Dimitry Wuyts <dimiw1337@hotmail.com>
2020-05-22 14:04:50 +0200

    full js course udemy

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,35 @@
+// var firstName = 'Dimi';
+// var lastName = 'wuyts';
+// var age = 29
+// var fullAge = true;
+// var job;
+// console.log(fullAge)
+// job = 'teacher';
+// console.log(job)
+
+/* ------------------------------------Variable mutation and Type coercion------------------------------------ */
+
+// var firstName = 'jhon';
+// var age =  29
+
+/* ------------------------------------Type coercion------------------------------------ */
+
+// console.log (firstName + ' ' + age)
+//
+// var job, isMarried;
+// job = 'teacher';
+// isMarried = false;
+
+/* ------------------------------------Variable mutation------------------------------------ */
+
+// age = 'twenty eight';
+// job = 'driver';
+//
+// alert(firstName + ' ' + age + ' year old ' + job + '. is he married ? ' + isMarried);
+//
+// var lastName = prompt('what is his last name?');
+//
+// console.log(firstName + ' ' + lastName)
+
+/* ------------------------------------Basic operators------------------------------------ */
+
